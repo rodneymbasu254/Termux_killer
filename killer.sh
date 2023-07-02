ad63b9a7c2bf24c3f24e0e04e29001b30e21d60b
Rodney Mbasu <rodneymbasu@gmail.com>
2023-07-02 19:01:09 +0300

    Update killer.sh

--- killer.sh
+++ killer.sh
@@ -1,1 +1,23 @@
+#! /bin/bash
 
+#We first call the user to input his/her name
+
+echo "What's your name"
+
+read name
+
+#Program ps aux
+
+ps aux
+
+echo "please enter the PID to be killed"
+
+read PID
+
+echo "enter kill sign"
+
+read sign
+
+kill -$sign $PID
+
+echo "$name successfully killed $PID"
